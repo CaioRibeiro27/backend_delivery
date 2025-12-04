f6127561809f603b204003ff28854d00df66e773
CaioRibeiro <caiopr0102@gmail.com>
2025-12-04 14:52:50 -0300

    Alterações de conexão

--- db.js
+++ db.js
@@ -1,19 +1,29 @@
 require("dotenv").config();
-const mysql = require("mysql2");
+const { Pool } = require("pg");
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const dbConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: 5432,
+    };
 
-db.getConnection((err, connection) => {
+const db = new Pool(dbConfig);
+
+db.connect((err, client, release) => {
   if (err) {
-    console.error("❌ Erro ao conectar ao banco:", err);
+    console.error("❌ Erro ao conectar ao banco:", err.message);
   } else {
-    console.log("✅ Conectado ao MySQL!");
-    connection.release();
+    console.log("✅ Conectado ao Banco de Dados com sucesso!");
+    release();
   }
 });
 
--- server.js
+++ server.js
@@ -2,24 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
+
 app.use(express.json());
 
-// Importando a rota
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const restaurantRoutes = require("./routes/restaurant");
 const orderRoutes = require("./routes/orders");
 
-// Usando as rotas
-app.use("/api/auth", authRoutes); // Login e Cadastro
-app.use("/api/user", userRoutes); // Perfil, Endereços e Cartões
-app.use("/api/restaurant", restaurantRoutes); // Coisas do Restaurante
-app.use("/api/orders", orderRoutes); // Pedidos
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/restaurant", restaurantRoutes);
+app.use("/api/orders", orderRoutes);
 
-// Rota genérica para testar
 app.get("/", (req, res) => {
   res.send("Servidor Delivery Rodando! 🚀");
 });
